7646037fb8bdf0c15dc537969155d8be564eeb0d
mehmetsagir <memetsagirint@gmail.com>
2021-11-23 23:53:03 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+const Home = () => {
+  return <div>Home Page</div>;
+};
+
+
+export default Home;
---
+++ styles/GlobalStyles.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+	:root {
+		--color-bg: #0E1117;
+		--color-primary: #151922;
+		--color-primary-hover: #191e26;
+		--color-text: #f1f1f1;
+		--color-text-secondary: #8b949e;
+	}
+  * {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font: inherit;
+		color: inherit;
+		outline: 0;
+	}
+	html, body {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-size: 14px;
+		height: 100%;
+	}
+	#__next {
+		height: 100%;
+	} 
+	body {
+		background: var(--color-bg);
+		color: var(--color-text);
+		height: 100%;
+	}
+	a {
+		text-decoration: none;
+	}
+	ul {
+		list-style: none;
+	}
+	button {
+		background: none;
+		border: none;
+	}
+	.btn {
+		background: var(--color-primary);
+    color: var(--color-text);
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 25px;
+    border-radius: 5px;
+    cursor: pointer;
+		white-space: nowrap;
+    transition: 200ms;
+
+    &:hover {
+      background: var(--color-primary-hover);
+    }
+	}
+`;
+
+export default GlobalStyles;
